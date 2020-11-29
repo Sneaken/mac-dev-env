d99869f16a2a05c24c57f226f25f7e6a8a451d42
Bin Jiang <bin.jiang@geneegroup.com>
2020-11-29 21:37:28 +0800

    finish the init script

--- init.bash
+++ init.bash
@@ -1,88 +1,169 @@
 #!/usr/bin/env bash
 
-echo "init your mac..."
-
-CURRENT_DIR=$(dirname "${BASH_SOURCE[0]}")
-CURRENT_DIR=$(cd "$CURRENT_DIR" && pwd)
-
-
-if ! command -v curl &> /dev/null; then
-    echo "curl could not be found"
-    exit
-fi
-
-# create work's directory
-dirs=("$HOME/Code/projects/shell" "$HOME/Code/env")
-
-for dir in "${dirs[@]}"; do
+function init_work_dirs() {
+  echo
+  echo 'init work directory...'
+  local dirs=("$HOME/Code/Projects/Sneaker" "$HOME/Code/Projects/front-end" "$HOME/Code/Projects/back-end" "$HOME/Code/env")
+  for dir in "${dirs[@]}"; do
     [ -d "$dir" ] || mkdir -p "$dir"
-done
+  done
+  echo 'work directory done...'
+}
 
-if ! command -v brew &>/dev/null; then
+function choose_default_shell() {
+  local choice
+  echo -n '
+please select the shell you use:
+1) bash
+2) zsh
+'
+  read -r choice
+  case $choice in
+  [bB]*)
+    SHELL_CONFIG_FILE='.bashrc'
+    ;;
+  [zZ]*)
+    SHELL_CONFIG_FILE='.zshrc'
+    if ! command -v zsh; then
+      echo 'zsh could not bu found'
+      exit
+    fi
+    chsh -s "$(command -v zsh)"
+    ;;
+  *)
+    echo
+    echo "Don't enter other shell! try again..."
+    choose_default_shell
+    return
+    ;;
+  esac
+}
+
+function init_brew() {
+  if ! command -v brew &>/dev/null; then
     echo "brew could not be found"
     echo "install brew, we will ask for user password"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-fi
+  fi
+}
 
-#echo "change default shell"
+function init_utils_by_brew() {
+  if ! command -v brew &>/dev/null; then
+    echo 'init utils fail...'
+    return
+  fi
 
-BREW_PREFIX=$(brew --prefix)
+  BREW_PREFIX=$(brew --prefix)
 
-# Install GNU core utilities (those that come with macOS are outdated).
-# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install coreutils
-ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
+  # Install GNU core utilities (those that come with macOS are outdated).
+  # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+  brew install coreutils
+  ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
 
-# Install some other useful utilities like `sponge`.
-#brew install moreutils
+  # Install some other useful utilities like `sponge`.
+  #brew install moreutils
 
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-#brew install findutils
+  # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+  #brew install findutils
 
-# Install GNU `sed`
-#brew install gnu-sed
+  # Install GNU `sed`
+  #brew install gnu-sed
 
-# Install a modern version of Bash.
-brew install bash
-brew install bash-completion2
+  if [ $SHELL_CONFIG_FILE == '.zshrc' ]; then
+    # Install a modern version of Zsh.
+    brew install zsh zsh-completions
+  else
+    # Install a modern version of Bash.
+    brew install bash bash-completion2
+  fi
 
-## Switch to using brew-installed bash as default shell
-#if ! grep -F -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
-#    echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells
-#    chsh -s "${BREW_PREFIX}/bin/bash"
-#fi
+  # common tools
+  brew install wget
+  brew install vim
+  brew install grep
+  brew install openssh
+  brew install screen
+  brew install ack
+  brew install git
+  brew install curl
 
+  brew install --cask oracle-jdk
+  #brew install maven
+  #brew install gradle
 
-cd "$CURRENT_DIR" || exit
+  brew install --cask docker
 
-# common tools
-#brew install wget
-#brew install vim
-brew install grep
-brew install openssh
-#brew install screen
-#brew install ack
-brew install git
-brew install curl
+  brew install --cask iterm2
 
-# openjdk
-brew install --cask oracle-jdk
-#brew install maven
-#brew install gradle
+  brew install --cask webstorm
+  brew install --cask visual-studio-code
 
-# docker
-brew install --cask docker
+  brew install nvm
+}
 
-# terminal
-brew install --cask iterm2
+function init_git() {
+  if ! command -v git &>/dev/null; then
+    echo 'git could not be found'
+    return
+  fi
+  local GIT_USER_NAME
+  local GIT_USER_EMAIL
 
-# IDE
-brew install --cask webstorm
-brew install --cask visual-studio-code
+  echo 'init your git config...'
 
-# nvm
-brew install nvm
+  echo -n 'please enter your username: '
+  read -r GIT_USER_NAME
+  git config --global user.name "$GIT_USER_NAME"
 
-#nvm install node
+  echo -n 'please enter your email: '
+  read -r GIT_USER_EMAIL
+  git config --global user.email "$GIT_USER_EMAIL"
 
-echo "done.."
+  # 生成公钥
+  ssh-keygen -o
+
+  echo 'git config done...'
+}
+
+function init_nvm() {
+  echo 'init nvm'
+  if ! [[ $NVM_DIR == "$HOME/.nvm" ]]; then
+    echo -n '
+export NVM_DIR="$HOME/.nvm"
+[ -s "$(brew --prefix)/opt/nvm/nvm.sh" ] && . "$(brew --prefix)/opt/nvm/nvm.sh" # This loads nvm
+[ -s "$(brew --prefix)/opt/nvm/etc/bash_completion.d/nvm" ] && . "$(brew --prefix)/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
+  ' >>"$HOME/${SHELL_CONFIG_FILE}"
+
+    # shellcheck source=/dev/nul
+    source "$HOME/${SHELL_CONFIG_FILE}"
+
+    nvm install node
+  fi
+
+  echo 'init nvm done'
+}
+
+function main() {
+  echo 'init your mac...'
+
+  init_work_dirs
+
+  if ! command -v curl &>/dev/null; then
+    echo 'curl could not be found'
+    exit
+  fi
+
+  choose_default_shell
+  init_brew
+  init_utils_by_brew
+
+  init_nvm
+  init_git
+
+  echo 'done...'
+
+  # shellcheck source=/dev/null
+  source "$HOME/${SHELL_CONFIG_FILE}"
+}
+
+main
